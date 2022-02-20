c3713665e5c325a702ef5b6aabd4ceb923249d4a
Wakoli Votes <votesdw@yahoo.com>
2022-02-20 08:10:54 +0300

    Add files via upload

---
+++ checkLeapYear.py
@@ -0,0 +1,10 @@
+# Project: Write a python program that checks whether a year is leap year or not.
+# You are required to use nested if...else to solve this problem. ]
+
+class check_leap:
+    def __init__(self, year):
+        self.year = year
+
+
+
+year_info = check_leap()
