5542339ad18225f7eeb220da01a9b2d72910c3bd
Wakoli Votes <votesdw@yahoo.com>
2022-02-20 13:49:52 +0300

    Add files via upload

--- checkLeapYear.py
+++ checkLeapYear.py
@@ -1,10 +1,23 @@
 # Project: Write a python program that checks whether a year is leap year or not.
 # You are required to use nested if...else to solve this problem. ]
+# Note: A leap year is exactly divisible by 4 except for century years (years ending with 00).
+# The century year is a leap year only if it is perfectly divisible by 400
 
-class check_leap:
-    def __init__(self, year):
-        self.year = year
+def check_leap():
+    # We need only integers as input, hence use try...except combination
+    try:
+        year = int(input("Hi, Enter the Year:"))
+        if year % 400 == 0:
+            print("This is Not a Leap year, this is a Century Year")
+        elif year % 4 == 0:
+            print(f"Yes, {year} is a leap year")
+        else:
+            print("This is not a leap year")
+    except ValueError:
+        print("Invalid Input, Please Enter a Number")
 
+        # If the user enters an invalid input, the function is called again
+        return check_leap()
 
-
-year_info = check_leap()
+# Function Call
+check_leap()
